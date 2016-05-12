80d0bfc8e4fc48b187242b9de09ffcb860a361af
Administrator <administrator@KF200-0132.KF200-0132>
2016-05-12 12:38:48 -0400

    Src and launch working

---
+++ src/sampler.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+#This sample code provides an easy to user interface to trigger sample taking
+
+import rospy
+from std_msgs.msg import Int8
+from std_msgs.msg import Byte
+flag = 0
+
+def callback(button):
+    global flag
+    if button.data == 1: 
+        flag = 450
+        rospy.loginfo(button.data)
+
+def sample_driver():
+    pub = rospy.Publisher('pump_ctl', Int8, queue_size=1)
+    rospy.init_node('sample_driver', anonymous=True)
+    rospy.Subscriber("samples", Byte, callback)
+    rate = rospy.Rate(10) # 10hz
+    controller = 0
+    
+    global flag
+    while not rospy.is_shutdown():
+        controller = 0
+        if flag > 0: 
+            print flag
+            controller = 9
+            flag = flag -1
+        if flag >= 350: controller = -9
+        #rospy.loginfo(controller)
+        pub.publish(controller)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        sample_driver()
+    except rospy.ROSInterruptException:
+        pass
